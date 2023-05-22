430c86153609b4a5df1a5e5cc5122299db30921f
Joshua Hoffmann <118402274+Jhoff15@users.noreply.github.com>
2023-05-22 14:29:07 +0200

    Add files via upload
    Upload

---
+++ helper.py
@@ -0,0 +1,78 @@
+import os
+from datetime import datetime
+
+##
+## Funktion zur erzeugen des Dictionary der Services-Datei
+##
+def get_system32_services_dict() -> dict: ## {port: service}
+    # Öffne Datei und speichere sie in Variable
+    fileread = open("C:\Windows\System32\drivers\etc\services", "r").read()
+    # Erstelle eine Liste der einzelnen Zeilen der Services-Datei
+    lines_string = fileread.splitlines()
+
+    # erstelle das Dictionary mit {port: service}
+    services_dict = {}
+    # Laufe durch alle Elemente in lines_string
+    for line in lines_string:
+        # Splite line beim Hashtag
+        split_by_hashtag = line.split(sep="#", maxsplit=2)[0]
+        # Splite neue Line in 3 Teile bei Leerzeichen
+        split_in_3 = split_by_hashtag.split(maxsplit=3)
+        if len(split_in_3) > 1:
+            # Trenne zweite Spalte bei "/" auf
+            split_port_protc = split_in_3[1].split(sep="/")
+            # es wird getestet ob bereits ein Eintrag für den Port existiert
+            # siehe port_scanner.py line 69 oder 
+            # https://stackoverflow.com/questions/68193407/how-do-i-check-whether-an-open-port-is-tcp-or-udp
+            if int(split_port_protc[0]) not in services_dict.keys():
+                # trage key, value par in Dictionary ein
+                services_dict[int(split_port_protc[0])] = split_in_3[0]
+    # gebe Dictionary zurück
+    return services_dict
+
+# universelle Funktion zur Eingabe -> Rekursive Funktion
+# Eingabe wird in Typ gecastet und erneute Eingabe gefordert
+# wenn ein Error auftritt.
+def get_input(typ: type, text: str, std_value: any):
+    # Input in var speichern 
+    var = input(text)
+
+    # checken ob Input leer ist -> Standartwert wird übernommen.
+    if not var:
+        return std_value
+    # Ist der Typ ungleich String wird gecastet und bei falscher Eingabe 
+    # eine neue Eingabe gefordert.
+    # Wenn typ == String wird Eingabe zurückgegeben
+    if typ != str:
+        try:
+            return typ(var)
+        except ValueError:
+            print(f"Falsche Eingabe! Geben Sie eine etwas vom Typ {typ} ein.")
+            return get_input(typ, text, std_value)
+    else:
+        return var
+
+# Startpunkt für Eingabe der benötigten Parameter
+# Hauptaufgabe = get_input() aufrufen
+def get_inputs() -> tuple[str, int, int]:
+    dst_host = "127.0.0.1"
+    start_port = 0
+    end_port = 1025
+    
+    print("Input startet... (keine Eingabe = Standart Werte)")
+    dst_host = get_input(str, f"Gebe die Host-IP ein({dst_host}): ", dst_host)
+    start_port = get_input(int, f"Gebe den Start Port ein({start_port}): ", start_port)
+    end_port = get_input(int, f"Gebe den End Port ein({end_port}): ", end_port)
+
+    return dst_host, start_port, end_port
+
+# kleine Helper Funktion die den Log-File und das Log-Folder erstellt.
+def create_file_name() -> str:
+    path = os.path.curdir
+    if not os.path.exists(f'{path}/logs'):
+        os.mkdir(f'{path}/logs')
+
+    time_now = datetime.now().strftime("%d%m%Y-%H-%M-%S")
+    file_name = f"logs\open-ports-{time_now}.log"
+
+    return file_name
---
+++ port_scanner.py
@@ -0,0 +1,105 @@
+import socket
+from concurrent import futures
+
+# Asynchrone Funktionen brauchen eine Funktion, die den Aufruf managed
+# -> hier find_all_async()
+def find_all_async(dst_host=str, start_port=int, end_port=int, verbose=False) -> list:
+    open_ports=[]
+
+    ## 
+    ## Sehr wichtiger HILFSBLOCK. Damit in port_checker keine Exception auftreten, wenn der 
+    ## Host nicht existiert, muss hier getestet werden ob eine grundsätzliche Verbindung mit 
+    ## dem Host möglich ist.
+    ##
+
+    # init eines Sockets
+    # Mit Socket können zum Beispiel Server-Client kommunikationen realisiert werden
+    # Socket kann z.B. mit connect eine TCP-Anfrage an einen Server schicken
+    # Bei Servern wird die Funktion socket.listen() aufgerufen, der Socket hört dann einen
+    # bestimmten Port ab und wartet auf einkommende Anfragen.
+    # https://realpython.com/python-sockets/
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Nach jedem connect oder generellen call wird die timeout Zeit abgewartet, bevor eine 
+    # Exception ausgelöst wird.
+    sock.settimeout(0.5)
+    #
+    #
+    #
+    #
+    #
+    #
+    ## Komplett Sinnlos so
+    ## connect_ex geht, aber besser umschreiben + erklärung
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+
+    try:
+        # sock.connect() versucht eine Verbindung mit der angegebenen IP/Port herzustellen
+        sock.connect_ex((dst_host, 80))
+        # wird keine Exception ausgelöst geht es hier weiter
+        # close() schließt Verbindung und beendet Socket
+    #Exception wenn Host nicht erreichbar ist.
+    except Exception as e:
+        print(f"Der Host existiert nicht. Exception: {e}")
+        return []
+    ##
+    ## Ende HILFSBLOCK
+    ##
+
+    # Erzeugung der Asynchronen Prozesse https://docs.python.org/3/library/concurrent.futures.html
+    with futures.ThreadPoolExecutor(max_workers=1000) as executor:
+        # erzeugt eine Liste mit den Typen list[Future[tuple[bool, int]]] , also wird der Wert noch erwartet, da die Werte
+        # vom Typ Future sind.
+        results = [
+            executor.submit(port_checker, dst_host=dst_host, testport=i, verbose=verbose, sock=sock) for i in range(start_port, end_port+1)
+        ]
+        # Sage dem executor das gewartet werden soll, bis ein Ergebnis zurückkommt
+        executor.shutdown(wait=True, cancel_futures=False)
+        
+        # Wenn thread fertig ist wird der Port der Liste open_ports hinzugefügt
+        # muss nicht in genauer Reihenfolge passieren
+        for thread in futures.as_completed(results):
+            # checken ob der Port offen ist -> Übergebene Bool-Variable
+            if thread.result()[0]: open_ports.append(thread.result()[1])
+    
+    # Liefere Liste zurück
+    return open_ports
+
+# Checkt ob ein TCP-Port auf einem Host-System erreichbar/offen ist.
+def port_checker(dst_host=str, testport=int, verbose=False, sock=socket) -> tuple[bool, int]:
+    # Dieser Print und in line 88 sind zur Verdeutlichung der Asynchronität
+    print(f'Port Checker on {testport} started...')
+
+    # um zweite If zu vermeiden wird port_open mit False deklariert und nur bei einem offenen Port überschrieben
+    port_open = False
+
+    # Socket für TCP Verbindungen erzeugt. Anmerkung ÄNDERN -------
+    # UDP macht keinen Sinn weil es keine Antwort sendet um zu versuchen eine Verbingund einzurichten
+    # -> es gibt keine wirklichen offenen UDP Ports
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #sock.settimeout(0.5)
+
+    # connect_ex() agiert genauso wie connect() aus Zeile 27, es wird aber entweder 0 oder 111 zurückgegeben
+    # 0: Erfolgreich -> Port offen
+    # 111: nicht Erfolgreich -> Port geschlossen oder anderer Fehler
+    # wesentlich schneller als connect, da kein try-catch Block benötigt wird.
+    val = sock.connect_ex((dst_host, testport))
+    # war die Verbindung erfolgreich wird port_open überschrieben
+    if val == 0:
+        port_open = True
+        print(f"Offener Port: {testport}")
+    sock.close()
+
+    # ist verbose = true wird diese zusätzliche Zeile ausgegeben.
+    if verbose and not port_open: print(f"Geschlossener Port: {testport}")
+
+    print(f'Port Checker on {testport} finished...')
+    # gebe ein Bool und den Port zurück
+    return port_open, testport
